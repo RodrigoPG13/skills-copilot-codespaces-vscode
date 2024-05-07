0f6f556e5ff17af2f975e07ccd36f20ce0b129c7
RodrigoPG13 <137918175+RodrigoPG13@users.noreply.github.com>
2024-05-07 23:22:04 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,53 @@
+//Create web server
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+
+http.createServer((req, res) => {
+    const urlObj = url.parse(req.url);
+    let filePath = "." + urlObj.pathname;
+    if (filePath === './') {
+        filePath = './index.html';
+    }
+
+    let extname = String(path.extname(filePath)).toLowerCase();
+    let contentType = 'text/html';
+    let mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    };
+
+    contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                fs.readFile('./404.html', (err, content) => {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf-8');
+                });
+            } else {
+                res.writeHead(500);
+                res.end('Sorry, check with the site admin for error: ' + err.code + ' ..\n');
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+}).listen(8080);
+console.log('Server running at http://):8080/');
